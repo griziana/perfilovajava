a4085fae436e773ec660c6875d61ffd92ca63ac3
shad.ever <shad.ever@33f16098-17c1-11df-8c82-d302e10fc01c>
2010-04-27 15:49:50 +0000

    № 6 + Попытки сделать №3

---
+++ JavaApplication5/src/s06/z06/zadacha06.java
@@ -0,0 +1,75 @@
+package s06.z06;
+
+import info.clearthought.layout.TableLayout;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Shadow2
+ * Date: 27.04.2010
+ * Time: 13:27:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class zadacha06 extends JPanel {
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.BLUE);
+
+        Calendar c = new GregorianCalendar();
+        double m = c.get(GregorianCalendar.MINUTE);
+        double h = c.get(GregorianCalendar.HOUR);
+        double s = c.get(GregorianCalendar.SECOND);
+
+        int x0 = 150;
+        int y0 = 150;
+        int R = 100;
+
+        g.drawOval(x0 - R, y0 - R, 2 * R, 2 * R);
+        g.drawLine(
+                x0,
+                y0,
+                (int) Math.round(x0 + 40 * Math.sin(((h * 60 + m) / 2) / 180 * Math.PI)),
+                (int) Math.round(y0 - 40 * Math.cos(((h * 60 + m) / 2) / 180 * Math.PI))
+        );
+
+        g.drawLine(
+                x0,
+                y0,
+                (int) Math.round(x0 + 70 * Math.sin((s * 6) / 180 * Math.PI)),
+                (int) Math.round(y0 - 70 * Math.cos((s * 6) / 180 * Math.PI))
+        );
+        g.drawLine(
+                x0,
+                y0,
+                (int) Math.round(x0 + 60 * Math.sin((m * 6) / 180 * Math.PI)),
+                (int) Math.round(y0 - 60 * Math.cos((m * 6) / 180 * Math.PI))
+        );
+    }
+
+    public static void main(String[] args) {
+        TableLayout t1 = new TableLayout(new double[]{TableLayout.FILL},
+                new double[]{TableLayout.FILL});
+        final zadacha06 c1 = new zadacha06();
+        JFrame f = new JFrame();
+        f.setLayout(t1);
+        f.setSize(300, 350);
+        f.add(c1, "0,0");
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Timer t = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                c1.repaint();
+            }
+        });
+        t.start();
+
+    }
+}
